0154bee2cf27f334d8bb4da3c2e06f2cf720e24f
Caique Carvalho <caique.carvalho@baianao.com.br>
2024-06-02 12:59:40 -0300

    adicionando o cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { startKeepalive } = require("./src/database/keepalive/keepalive");
+const cors = require('cors');
 
 const router = require('./src/routes/route');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api', router);
 
 const port = 21066
