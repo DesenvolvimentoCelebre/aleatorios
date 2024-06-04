742a3a2f5b08e02796dc78023cf4eabe7f4bfeb4
Caique Carvalho <caique.carvalho@baianao.com.br>
2024-06-03 23:18:35 -0300

    adicionando  strategia jwt

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -3,15 +3,18 @@ const JWTStrategy = require("passport-jwt").Strategy;
 const ExtractJWT = require("passport-jwt").ExtractJwt;
 
 passport.use(
-    new JWTStrategy(
-      {
-        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-        secretOrKey: "token", // Replace this with your JWT secret
-      },
-      (jwtPayload, done) => {
-        // Your logic to validate the user based on the JWT payload
-      }
-    )
-  );
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: "token", // Replace this with your JWT secret
+    },
+    (jwtPayload, done) => {
+      // Your logic to validate the user based on the JWT payload
+      // For example, you could retrieve the user from the database
+      const user = { id: jwtPayload.userId, name: jwtPayload.name };
+      done(null, user); // Call done() with the user object
+    }
+  )
+);
 
-  module.exports = passport;
+module.exports = passport;
